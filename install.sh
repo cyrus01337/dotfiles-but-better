6833b7ddf29e855ce80d515711c2fa1a192ec15e
cyrus01337 <cyrus21337@gmail.com>
2024-10-27 18:21:21 +0000

    Special-case installations more explicitly

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-REQUIRED_PACKAGES=(curl git stow fish starship tmux docker)
+REQUIRED_PACKAGES=(curl git stow fish tmux)
 SYSTEM_PACKAGE_MANAGERS=(apt-get dnf pacman)
 DEFAULT_SHELL="fish"
-INSTALLING_DOCKER_FLAG=false
-INSTALLING_STARSHIP_FLAG=false
 
 exists() {
     which $1 &> /dev/null
@@ -28,13 +26,7 @@ get_packages_to_be_installed_with() {
 
     for package in ${REQUIRED_PACKAGES[@]}; do
         if ! exists $package; then
-            if [[ $package = "docker" ]]; then
-                INSTALLING_DOCKER_FLAG=true
-            elif [[ $package = "starship" ]]; then
-                INSTALLING_STARSHIP_FLAG=true
-            else
-                echo $package
-            fi
+            echo $package
         fi
     done
 }
@@ -54,14 +46,6 @@ install_using_system_package_manager() {
     system_package_manager=$1
     arguments=("${@:2}")
 
-    if [[ $INSTALLING_DOCKER_FLAG = true ]]; then
-        install_docker
-    fi
-
-    if [[ $INSTALLING_STARSHIP_FLAG = true ]]; then
-        install_starship
-    fi
-
     if [[ $system_package_manager = "apt-get" ]]; then
         sudo $system_package_manager install -y ${arguments[@]}
     elif [[ $system_package_manager = "dnf" ]]; then
@@ -69,14 +53,24 @@ install_using_system_package_manager() {
     elif [[ $system_package_manager = "pacman" ]]; then
         sudo $system_package_manager -S --noconfirm ${arguments[@]}
     fi
+
+    if ! exists starship; then
+        install_starship
+    fi
+
+    if ! exists docker; then
+        install_docker
+    fi
 }
 
 system_package_manager=$(get_system_package_manager)
 packages_to_be_installed=($(get_packages_to_be_installed_with $system_package_manager))
 
 install_using_system_package_manager $system_package_manager ${packages_to_be_installed[@]}
+
 mkdir -p ~/Projects/personal
 git clone https://github.com/cyrus01337/dotfiles-but-better.git ~/Projects/personal/dotfiles
+
 cd ~/Projects/personal/dotfiles
 git submodule update --init --recursive
 stow . -t ../../../
