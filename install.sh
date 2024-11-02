d8d522244915baf823e0aec514e5c36d5a6ccc7e
cyrus01337 <cyrus21337@gmail.com>
2024-11-02 08:58:32 +0000

    Use curl or wget fallback when requesting files

--- install.sh
+++ install.sh
@@ -2,7 +2,16 @@
 set -e
 
 install_docker() {
-    sudo sh -c "$(curl -fsSL https://get.docker.com)"
+    if which curl &> /dev/null; then
+        sudo sh -c "$(curl -fsSL https://get.docker.com)"
+    elif which wget &> /dev/null; then
+        sudo sh -c "$(wget -qO - https://get.docker.com)"
+    else
+        echo "You must have curl/wget installed to run this script"
+
+        exit 127
+    fi
+
     sudo usermod -aG docker $USER
 
     sudo systemctl start --quiet docker.service
@@ -12,5 +21,10 @@ if ! which docker &> /dev/null; then
     install_docker
 fi
 
-curl -fsSL https://github.com/cyrus01337/dotfiles-but-better/raw/refs/heads/main/.bashrc -o $HOME/.bashrc
-source .bashrc
+if which curl &> /dev/null; then
+    curl -fsSL https://github.com/cyrus01337/dotfiles-but-better/raw/refs/heads/main/.bashrc -o $HOME/.bashrc
+elif which wget &> /dev/null; then
+    wget -qO $HOME/.bashrc https://github.com/cyrus01337/dotfiles-but-better/raw/refs/heads/main/.bashrc
+fi
+
+source $HOME/.bashrc
