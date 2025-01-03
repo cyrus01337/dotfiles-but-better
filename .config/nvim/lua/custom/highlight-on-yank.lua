baa524a1dbcccdf9534fdb9de91f9de9a66043dd
cyrus01337 <cyrus01337@gmail.com>
2025-01-03 06:49:41 +0000

    Remove static Neovim configuration

--- .config/nvim/lua/custom/highlight-on-yank.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-})
-
--- .config/nvim/lua/custom/lib/constants.lua
+++
@@ -1,8 +0,0 @@
-return {
-    EMPTY = "",
-    FILE_MANAGER = "Oil",
-    THEME = {
-        package = "dracula/vim",
-        name = "dracula",
-    },
-}
