e7f93314cbdafbd6a50daf3a289a1378a1b552a0
cyrus01337 <cyrus01337@gmail.com>
2025-01-06 20:41:25 +0000

    Readily edit Tmux configuration

--- .config/fish/conf.d/aliases/utility.fish
+++ .config/fish/conf.d/aliases/utility.fish
@@ -1,7 +1,8 @@
 #!/usr/bin/env fish
-set FISH_CONFIG_PATH "$__fish_config_dir/config.fish"
+set FISH_CONFIGURATION_PATH "$__fish_config_dir/config.fish"
+set TMUX_CONFIGURATION_PATH "$HOME/.config/tmux/tmux.conf"
 
-alias f='n $__fish_config_dir/ -c "nvim ." && . $FISH_CONFIG_PATH'
+alias f='n $__fish_config_dir/ -c "nvim ." && . $FISH_CONFIGURATION_PATH'
 alias lsa="ls -a"
 alias lsal="ls -al"
 alias lsl="ls -l"
@@ -11,4 +12,5 @@ alias schmod="sudo chmod"
 alias schown="sudo chown"
 alias sshq="ssh -q"
 alias scpq="scp &> /dev/null"
+alias t='n $TMUX_CONFIGURATION_PATH && tmux source $TMUX_CONFIGURATION_PATH'
 alias tree="tree -C"
