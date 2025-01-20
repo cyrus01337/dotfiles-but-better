6921d794980d936c0f3df8cbdf44c95d62e1b832
cyrus01337 <cyrus01337@gmail.com>
2025-01-20 02:12:15 +0000

    Pick current group's project using group as context

--- .config/fish/conf.d/modules/projects.fish
+++ .config/fish/conf.d/modules/projects.fish
@@ -3,41 +3,59 @@ set PROJECTS_DIRECTORY "$HOME/Projects/"
 
 if test -d $PROJECTS_DIRECTORY
     function projects
-        set EMPTY ""
         set INTERRUPTED_OR_FATAL_ERROR 130
         set -l FZF_FLAGS -1 --cycle --no-mouse --no-scrollbar --ellipsis ...
-        set group (ls -r $PROJECTS_DIRECTORY | fzf $FZF_FLAGS)
+        set -l groups (ls $PROJECTS_DIRECTORY)
+        set selected_group ""
 
-        if test "$group" = "$EMPTY"
-            return $INTERRUPTED_OR_FATAL_ERROR
+        if test (count $groups) = 0
+            return 1
         end
 
-        set -l objects (ls -l $PROPTECTS_DIRECTORY/$group)
+        for group in $groups
+            if not string match -q "*$group*" $PWD
+                continue
+            end
+
+            set selected_group $group
+
+            break
+        end
+
+        if test "$selected_group" = ""
+            set selected_group (ls -r $PROJECTS_DIRECTORY | fzf $FZF_FLAGS)
+
+            if test "$selected_group" = ""
+                return $INTERRUPTED_OR_FATAL_ERROR
+            end
+        end
+
+        set -l objects (ls -l $PROPTECTS_DIRECTORY/$selected_group)
 
         if test (count $objects) = 0
             return 1
         end
 
-        set project (ls -r "$PROJECTS_DIRECTORY/$group" | fzf $FZF_FLAGS)
+        set selected_project (ls -r "$PROJECTS_DIRECTORY/$selected_group" | fzf $FZF_FLAGS)
 
-        if test "$project" = "$EMPTY"
+        if test "$selected_project" = ""
             return $INTERRUPTED_OR_FATAL_ERROR
         end
 
         # TODO: Create a modular system allowing for special-casing certain
-        # directories, namely for granular management like below
-        if test "$project" != "lamna"
-            cd "$PROJECTS_DIRECTORY/$group/$project"
+        # directories, namely for granular management of special-cases e.g. below
+        if test "$selected_project" != "lamna"
+            cd "$PROJECTS_DIRECTORY/$selected_group/$selected_project"
 
             return 0
         end
 
-        set subproject (ls -r "$PROJECTS_DIRECTORY/$group/$project" | fzf $FZF_FLAGS)
+        set selected_subproject (ls -r "$PROJECTS_DIRECTORY/$selected_group/$selected_project" | fzf $FZF_FLAGS)
 
-        if test "$subproject" = "$EMPTY"
+        if test "$selected_subproject" = ""
             return 0
         end
 
-        cd "$PROJECTS_DIRECTORY/$group/$project/$subproject"
+        cd "$PROJECTS_DIRECTORY/$selected_group/$selected_project/$selected_subproject"
     end
 end
