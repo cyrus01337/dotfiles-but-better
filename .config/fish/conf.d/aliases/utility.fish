149c9231950ba1400f7d6856aa43ff8c6165143d
cyrus01337 <cyrus01337@gmail.com>
2025-01-20 02:12:33 +0000

    Create aliases for entering Fish/Neovim directories

--- .config/fish/conf.d/aliases/utility.fish
+++ .config/fish/conf.d/aliases/utility.fish
@@ -1,11 +1,14 @@
 #!/usr/bin/env fish
 set FISH_CONFIGURATION_PATH "$__fish_config_dir/config.fish"
+set NEOVIM_CONFIGURATION_PATH "$HOME/.config/nvim/"
 set TMUX_CONFIGURATION_PATH "$HOME/.config/tmux/tmux.conf"
 
 alias f='n $__fish_config_dir/ -c "nvim ." && . $FISH_CONFIGURATION_PATH'
+alias fd="cd $__fish_config_dir/"
 alias lsa="ls -a"
 alias lsal="ls -al"
 alias lsl="ls -l"
+alias nd="cd $NEOVIM_CONFIGURATION_PATH"
 alias q="exit"
 alias r="clear && . $FISH_CONFIG_PATH"
 alias schmod="sudo chmod"
