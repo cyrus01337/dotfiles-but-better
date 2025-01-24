52efe465bcbb5b7d73e0568a132595c4a03e91ca
cyrus01337 <cyrus01337@gmail.com>
2025-01-24 08:45:11 +0000

    Migrate aliases to Tmux keybinds

--- .config/fish/conf.d/aliases/utility.fish
+++ .config/fish/conf.d/aliases/utility.fish
@@ -1,9 +1,6 @@
 #!/usr/bin/env fish
-set FISH_CONFIGURATION_PATH "$__fish_config_dir/config.fish"
 set NEOVIM_CONFIGURATION_PATH "$HOME/.config/nvim/"
-set TMUX_CONFIGURATION_PATH "$HOME/.config/tmux/tmux.conf"
 
-alias f='n $__fish_config_dir/ -c "nvim ." && . $FISH_CONFIGURATION_PATH'
 alias fd="cd $__fish_config_dir/"
 alias lsa="ls -a"
 alias lsl="ls -l"
@@ -15,5 +12,4 @@ alias schmod="sudo chmod"
 alias schown="sudo chown"
 alias sshq="ssh -q"
 alias scpq="scp &> /dev/null"
-alias t='n $TMUX_CONFIGURATION_PATH && tmux source $TMUX_CONFIGURATION_PATH'
 alias tree="tree -C"
