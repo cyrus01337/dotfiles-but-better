25307254a45c5f746bfcfd6520d4e403d4977775
cyrus01337 <cyrus01337@gmail.com>
2025-01-27 07:55:46 +0000

    Remove Fugitive over Lazygit

--- .config/nvim/lua/custom/features/git-integration.lua
+++ .config/nvim/lua/custom/features/git-integration.lua
@@ -1,24 +1,5 @@
 return {
     {
-        "tpope/vim-fugitive",
-        cmd = { "Git" },
-        keys = {
-            { "ga", "<CMD>Git<CR>", remap = true },
-            { "gch", ":Git checkout " },
-            { "gca", "<CMD>Git commit --amend --no-edit<CR>" },
-            { "gcm", "<CMD>Git commit<CR>" },
-            { "gi", "<CMD>Git init<CR>" },
-            { "gism", "<CMD>Git submodule update --init --recursive<CR>" },
-            { "gm", ":Git merge " },
-            { "gra", ":Git remote add " },
-            { "gpl", ":Git pull " },
-            { "gps", ":Git push " },
-            { "gsm", ":Git submodule " },
-            { "gusm", "<CMD>Git submodule update --recursive --remote" },
-        },
-        config = true,
-    },
-    {
         "wintermute-cell/gitignore.nvim",
         dependencies = {
             "nvim-telescope/telescope.nvim",
