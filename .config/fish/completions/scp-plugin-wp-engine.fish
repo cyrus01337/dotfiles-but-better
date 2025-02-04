85a31efd60fceba52116215fdd532dc03995c83d
cyrus01337 <cyrus01337@gmail.com>
2025-02-04 11:55:49 +0000

    Fancily refine 2nd argument's completion condition

--- .config/fish/completions/scp-plugin-wp-engine.fish
+++ .config/fish/completions/scp-plugin-wp-engine.fish
@@ -1,8 +1,13 @@
 #!/usr/bin/env fish
-set -l ENVIRONMENTS_DIRECTORIES (ls $__fish_config_dir/completions/environments/)
 set CURRENT_DIRECTORY (pwd)
-set CURRENT_DIRECTORIES (path filter --type dir (ls $CURRENT_DIRECTORY))
+set -l ENVIRONMENTS_DIRECTORIES (ls $__fish_config_dir/completions/environments/)
+# TODO: Refine plugin directory detection by searching for PHP project artifacts
+set -l CURRENT_DIRECTORIES (path filter --type dir (ls $CURRENT_DIRECTORY)) "./"
 
 complete --command scp-plugin-wp-engine --no-files
 complete --command scp-plugin-wp-engine --condition "__fish_use_subcommand" --arguments "$CURRENT_DIRECTORIES"
-complete --command scp-plugin-wp-engine --require-parameter --condition "__fish_seen_subcommand_from $CURRENT_DIRECTORIES" --arguments "$ENVIRONMENTS_DIRECTORIES"
+complete \
+    --command scp-plugin-wp-engine \
+    --require-parameter \
+    --condition "__fish_seen_subcommand_from $CURRENT_DIRECTORIES; and not __fish_seen_subcommand_from $ENVIRONMENTS_DIRECTORIES" \
+    --arguments "$ENVIRONMENTS_DIRECTORIES"
