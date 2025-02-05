f48cff16fffe7e30dde0712940a51bce0ae7fc40
cyrus01337 <cyrus01337@gmail.com>
2025-02-05 01:25:49 +0000

    Restrict completions to 1 positional argument

--- .config/fish/completions/ssh-wp-engine.fish
+++ .config/fish/completions/ssh-wp-engine.fish
@@ -2,4 +2,4 @@
 set -l ENVIRONMENTS_DIRECTORY (ls $__fish_config_dir/completions/environments/)
 
 complete --command ssh-wp-engine --no-files
-complete --command ssh-wp-engine --require-parameter --arguments "$ENVIRONMENTS_DIRECTORY"
+complete --command ssh-wp-engine --exclusive --condition "__fish_is_first_arg" --arguments "$ENVIRONMENTS_DIRECTORY"
