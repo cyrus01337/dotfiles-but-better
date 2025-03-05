b94c2be2cfa23adc562c48c4c11ff54eed99b7b9
cyrus01337 <cyrus01337@gmail.com>
2025-03-05 13:35:44 +0000

    Add update helper

--- .config/fish/conf.d/modules/spicetify.fish
+++ .config/fish/conf.d/modules/spicetify.fish
@@ -3,6 +3,11 @@ set SPICETIFY_PATH "$HOME/.spicetify"
 
 if test -d $SPICETIFY_PATH
     fish_add_path "$SPICETIFY_PATH"
+
+    function update-spicetify
+        spicetify update
+        spicetify backup apply
+    end
 end
 
 return 0
