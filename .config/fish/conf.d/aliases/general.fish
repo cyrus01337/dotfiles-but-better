848d1714c30f491858a430a802e6a66f9fe3ae71
cyrus01337 <cyrus01337@gmail.com>
2025-03-27 16:44:14 +0000

    Create utility for swapping locations of and linking objects

---
+++ .config/fish/conf.d/aliases/general.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+function switch-and-link
+    set target $argv[1]
+    set destination $argv[2]
+
+    if not test -f $target; and not test -d $target
+        echo "target must be an existing file or directory"
+
+        return 1
+    end
+
+    if test -z $destination; or not test -d $destination
+        set destination $PWD
+    end
+
+    set filename (basename $target)
+
+    mv $target "$destination/$filename"
+    ln -s "$destination/$filename" $target
+end
