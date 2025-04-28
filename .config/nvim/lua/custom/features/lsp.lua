f710ae657debe6f3a2f6b05f43d43559788c5236
cyrus01337 <cyrus01337@gmail.com>
2025-04-28 22:14:08 +0100

    Double formatters leeway

--- .config/nvim/lua/custom/features/lsp.lua
+++ .config/nvim/lua/custom/features/lsp.lua
@@ -308,7 +308,7 @@ return {
             },
             format_on_save = {
                 lsp_format = "fallback",
-                timeout_ms = 500,
+                timeout_ms = 1000,
             },
         },
     },
