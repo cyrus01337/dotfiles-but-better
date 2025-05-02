76d73aad41cc48ff2712097aa65150007c25aec4
cyrus01337 <cyrus01337@gmail.com>
2025-05-02 23:02:15 +0100

    Add security warning for passwordless prompts

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -5,6 +5,7 @@
     ...
 }:
 
+        url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
 {
     imports = [
         ./hardware-configuration.nix
@@ -211,6 +212,15 @@
         pkgs.xterm
     ];
 
+    # Allow passwordless prompts due to my user being in the wheel group
+    #
+    # Word of warning - never do this on any system ever. I work exclusively in
+    # VMs at the time of writing this, so long as it has a connection to the
+    # internet or access to potentially sensitive data you must always set
+    # passwords for elevated privileges as a general rule of thumb. I simply
+    # don't care for this VM, however, if there is ever the intention of
+    # carrying this over to bare metal, this option will be removed in it's
+    # entirety.
     security.sudo.wheelNeedsPassword = false;
     users.users.cyrus = {
         description = "cyrus";
