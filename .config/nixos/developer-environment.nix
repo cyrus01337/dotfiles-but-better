3f918e0a5960e25c588d07f30ef046897ad8f71c
cyrus01337 <cyrus01337@gmail.com>
2025-05-15 12:49:46 +0100

    Convert system packages to user packages

--- .config/nixos/developer-environment.nix
+++ .config/nixos/developer-environment.nix
@@ -3,19 +3,21 @@
         ./neovim.nix
     ];
 
-    environment.systemPackages = with pkgs; [
-        bun
-        docker
-        docker-compose
-        iproute2
-        jq
-        lazydocker
-        nodejs
-        parallel
-        php83
-        python311
-        vscode
-    ];
+    home = {
+        packages = with pkgs; [
+            bun
+            docker
+            docker-compose
+            iproute2
+            jq
+            lazydocker
+            nodejs
+            parallel
+            php83
+            python311
+            vscode
+        ];
+    };
 
     services.gpg-agent = {
         defaultCacheTtl = 1800;
--- .config/nixos/neovim.nix
+++ .config/nixos/neovim.nix
@@ -1,81 +1,73 @@
-{pkgs, ...}: {
-    environment.systemPackages = with pkgs; [
-        neovim
-
-        alejandra
-        ast-grep
-        cargo
-        eslint
-        fd
-        fzf
-        gcc
-        gnumake
-        go
-        julia
-        lua51Packages.jsregexp
-        lua51Packages.lua
-        lua51Packages.luarocks
-        nodePackages.prettier
-        php83Packages.composer
-        php83Packages.php-cs-fixer
-        prettierd
-        python311Packages.black
-        python311Packages.isort
-        ripgrep
-        stylua
-        typescript-language-server
-        vscode-langservers-extracted
-        zulu
-
-        (vimPlugins.nvim-treesitter.withPlugins (p:
-            with p; [
-                # meta
-
-                ## (neo)vim
-                vim
-                vimdoc
-                regex
-                markdown_inline
-
-                ## project management
-                gitignore
-                gitcommit
-                markdown
-
-                # web dev
-
-                ## front-end
-                html
-                css
-                javascript
-                vim-jsx-typescript
-                typescript
-                astro
-
-                ## back-end
-                php
-                sql
-
-                # dev-ops
-                dockerfile
-
-                # software/cli
-                bash
-                python
+{
+    pkgs,
+    ...
+}: {
+    programs.neovim = {
+        enable = true;
+        extraPackages = let
+            luapkgs = with pkgs.lua51Packages; [
+                jsregexp
                 lua
+                luarocks
+            ];
+        in
+            with pkgs;
+                luapkgs
+                ++ [
+                    alejandra
+                    ast-grep
+                    cargo
+                    # eslint
+                    gcc
+                    gnumake
+                    go
+                    julia
+                    nodePackages.prettier
+                    php83Packages.composer
+                    php83Packages.php-cs-fixer
+                    prettierd
+                    stylua
+                    typescript-language-server
+                    vscode-langservers-extracted
+                    zulu
 
-                # general
-                go
-                nix
-
-                # configuration formats
-                json
-                jsonc
-                yaml
-                toml
-
-                # documentation
-                markdown
-            ]))
-    ];
+                    (vimPlugins.nvim-treesitter.withPlugins (epkgs:
+                        with epkgs; [
+                            astro
+                            bash
+                            css
+                            dockerfile
+                            gitcommit
+                            gitignore
+                            go
+                            html
+                            javascript
+                            json
+                            jsonc
+                            lua
+                            markdown
+                            markdown
+                            markdown_inline
+                            nix
+                            php
+                            python
+                            regex
+                            sql
+                            toml
+                            typescript
+                            vim
+                            vim-jsx-typescript
+                            vimdoc
+                            yaml
+                        ]))
+                ];
+        extraPython3Packages = pypkgs:
+            with pypkgs; [
+                black
+                isort
+            ];
+        withNodeJs = true;
+        withPython3 = true;
+    };
+    xdg.configFile."nvim".source = let dotfiles = ./..; in dotfiles + "/nvim";
 }
