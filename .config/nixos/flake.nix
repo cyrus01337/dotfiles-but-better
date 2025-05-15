e4c59a9f5bc45f4e4b2aff39f7d484327562f225
cyrus01337 <cyrus01337@gmail.com>
2025-05-15 09:54:20 +0100

    Add home-manager for the third time

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -2,12 +2,32 @@
     description = "NixOS Configuration (but a flake!)";
 
     inputs = {
+        home-manager = {
+            url = "github:nix-community/home-manager";
+
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
-    outputs = {nixpkgs, ...} @ inputs: {
+    outputs = {
+        home-manager,
+        nixpkgs,
+        ...
+    } @ inputs: {
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
             modules = [
+                home-manager.nixosModules.home-manager
+                {
+                    home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+
+                        users.cyrus = ./home.nix;
+                    };
+                }
+
                 ./configuration.nix
             ];
             system = "x86_64-linux";
---
+++ .config/nixos/home.nix
@@ -0,0 +1,9 @@
+{
+    home = {
+        homeDirectory = "/home/cyrus";
+        stateVersion = "24.05";
+        username = "cyrus";
+    };
+
+    programs.home-manager.enable = true;
+}
