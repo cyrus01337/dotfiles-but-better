812d4a6052cf2991058609265678b95686d98307
cyrus01337 <cyrus01337@gmail.com>
2025-05-15 15:36:28 +0100

    Tidy

--- .config/nixos/shell.nix
+++ .config/nixos/shell.nix
@@ -23,10 +23,14 @@ in
             starship.enable = true;
         };
 
-        home.file = {
-            ".inputrc".text = builtins.readFile ./../../.inputrc;
-            ".profile".text = builtins.readFile ./../../.profile;
+        home = {
+            file = {
+                ".inputrc".text = builtins.readFile ./../../.inputrc;
+                ".profile".text = builtins.readFile ./../../.profile;
+            };
         };
-        xdg.configFile."fish".source = dotfiles-xdg + "/fish";
-        xdg.configFile."starship".source = dotfiles-xdg + "/starship";
+        xdg.configFile = {
+            "fish".source = dotfiles-xdg + "/fish";
+            "starship".source = dotfiles-xdg + "/starship";
+        };
     }
