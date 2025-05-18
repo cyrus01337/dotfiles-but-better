bc5b59aab4baa7249d948d041f69ad3cc6dbbfc8
cyrus01337 <cyrus01337@gmail.com>
2025-05-18 09:10:59 +0100

    Unarchive partitions

---
+++ .config/nixos/user/applications/neovim.nix
@@ -0,0 +1,82 @@
+{pkgs, ...}: let
+    dotfiles = ./../../..;
+in
+    with pkgs; {
+        programs.neovim = {
+            enable = true;
+            extraPackages = let
+                runtime-packages = [
+                    bun
+                    go
+                    php83
+                    python311
+                    zulu
+                ];
+                lua-packages = with lua51Packages; [
+                    jsregexp
+                    lua
+                ];
+                developer-tool-packages = [
+                    alejandra
+                    nodePackages.prettier
+                    php83Packages.composer
+                    php83Packages.php-cs-fixer
+                    prettierd
+                    stylua
+                ];
+                treesitter-packages = [
+                    (vimPlugins.nvim-treesitter.withPlugins
+                    (epkgs:
+                        with epkgs; [
+                            astro
+                            bash
+                            css
+                            dockerfile
+                            gitcommit
+                            gitignore
+                            go
+                            html
+                            javascript
+                            json
+                            jsonc
+                            lua
+                            markdown
+                            markdown
+                            markdown_inline
+                            nix
+                            php
+                            python
+                            regex
+                            sql
+                            toml
+                            typescript
+                            vim
+                            vim-jsx-typescript
+                            vimdoc
+                            yaml
+                        ]))
+                ];
+                lsp-packages = [
+                    typescript-language-server
+                    vscode-langservers-extracted
+                ];
+                packages-to-be-organised = [
+                    ast-grep
+                ];
+            in
+                runtime-packages
+                ++ lua-packages
+                ++ developer-tool-packages
+                ++ treesitter-packages
+                ++ lsp-packages
+                ++ packages-to-be-organised;
+            extraPython3Packages = pypkgs:
+                with pypkgs; [
+                    black
+                    isort
+                ];
+            withNodeJs = true;
+            withPython3 = true;
+        };
+        xdg.configFile."nvim".source = dotfiles + "/nvim";
+    }
