b3e6fb8c609ae76c0f485ae258636609fdface77
cyrus01337 <cyrus01337@gmail.com>
2025-05-18 17:01:23 +0100

    Simplify

--- .config/nixos/user/shell.nix
+++ .config/nixos/user/shell.nix
@@ -3,34 +3,35 @@
     pkgs,
     ...
 }: let
+    inherit lib;
+
     dotfiles = ./../../..;
     dotfiles-xdg = dotfiles + "/.config";
-in
-    with lib; {
-        imports = [
-            ./applications/tmux.nix
+in {
+    imports = [
+        ./applications/tmux.nix
+    ];
+
+    home = {
+        file = {
+            ".inputrc".text = builtins.readFile (dotfiles + "/.inputrc");
+            ".profile".text = builtins.readFile (dotfiles + "/.profile");
+        };
+        packages = [
+            pkgs.fish
         ];
-
-        home = {
-            file = {
-                ".inputrc".text = builtins.readFile (dotfiles + "/.inputrc");
-                ".profile".text = builtins.readFile (dotfiles + "/.profile");
-            };
-            packages = [
-                pkgs.fish
-            ];
-        };
-        programs = {
-            bash = {
-                enable = true;
-                initExtra = builtins.readFile (dotfiles + "/.bashrc");
-            };
-
-            # TODO: Migrate Fish configuration to Nix because this doesn't work
-            # as expected
-            fish.generateCompletions = true;
-            starship.enable = true;
+    };
+    programs = {
+        bash = {
+            enable = true;
+            initExtra = builtins.readFile (dotfiles + "/.bashrc");
         };
 
-        xdg.configFile."starship".source = dotfiles-xdg + "/starship";
-    }
+        # TODO: Migrate Fish configuration to Nix because this doesn't work
+        # as expected
+        fish.generateCompletions = true;
+        starship.enable = true;
+    };
+
+    xdg.configFile."starship".source = dotfiles-xdg + "/starship";
+}
