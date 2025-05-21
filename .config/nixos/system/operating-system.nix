5aff65d73282706e758281be8797f4e3a0f67ffa
cyrus01337 <cyrus01337@gmail.com>
2025-05-21 15:45:56 +0100

    Elaborate on disabling logrotate from checking configs

--- .config/nixos/system/operating-system.nix
+++ .config/nixos/system/operating-system.nix
@@ -31,6 +31,8 @@
         XDG_STATE_HOME = "$HOME/.local/state";
     };
     nixpkgs.config.allowUnfree = true;
-    # TODO: Elaborate
+    # NixOS installation fails in a chroot environment due to the inability to
+    # check a config file for logrotate, so we disable it temporarily to allow
+    # the installation first then remove the option later
     services.logrotate.checkConfig = false;
 }
