87b2a34797335d20e1e13bb7b947cf39635c9875
cyrus01337 <cyrus01337@gmail.com>
2025-05-29 05:08:41 +0100

    Modernise conditions for Fish

--- .config/fish/conf.d/modules/pyenv.fish
+++ .config/fish/conf.d/modules/pyenv.fish
@@ -3,11 +3,11 @@ set -x PYENV_ROOT "$HOME/.local/pyenv"
 set -x PYENV_VIRTUALENV_DISABLE_PROMPT true
 set VENV_ACTIVATION_SCRIPT ".venv/bin/activate"
 
-if [ -d "$PYENV_ROOT" ]
+if test -d "$PYENV_ROOT"
     fish_add_path "$PYENV_ROOT/bin"
 
     eval "$(pyenv init -)" &> /dev/null
     eval "$(pyenv virtualenv-init -)" &> /dev/null
-else if [ -f $VENV_ACTIVATION_SCRIPT ]
+else if test -f $VENV_ACTIVATION_SCRIPT
     source $VENV_ACTIVATION_SCRIPT
 end
