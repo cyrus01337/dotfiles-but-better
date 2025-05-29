95e909fc85dbd37319839315c5e70c5dddf60ba1
cyrus01337 <cyrus01337@gmail.com>
2025-05-29 05:23:59 +0100

    Correct condition

--- .config/fish/conf.d/modules/fnm.fish
+++ .config/fish/conf.d/modules/fnm.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 set FNM_DIRECTORY "$HOME/.local/share/fnm"
 
-if command -q fnm; and test -d "$FNM_DIRECTORY"
+if command -q fnm; or test -d "$FNM_DIRECTORY"
     fish_add_path "$FNM_DIRECTORY"
 
     fnm env --use-on-cd --shell fish | source
