99d06d1f522f3305d4135f282bc6701be1b4a02c
cyrus01337 <cyrus01337@gmail.com>
2025-05-31 07:47:04 +0100

    Fix Bashism

--- .config/fish/conf.d/modules/cargo.fish
+++ .config/fish/conf.d/modules/cargo.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 set -x CARGO_HOME "$HOME/.local/share/cargo"
-set -x RUSTUP_HOME="$HOME/.local/share/rustup"
+set -x RUSTUP_HOME "$HOME/.local/share/rustup"
 set CARGO_ENVIRONMENT_PATH "$CARGO_HOME/env.fish"
 
 if test -f $CARGO_ENVIRONMENT_PATH
