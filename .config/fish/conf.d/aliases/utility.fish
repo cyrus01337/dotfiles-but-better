67ac11ef28fcc1d9d93a9a0578108059279a1852
cyrus01337 <cyrus01337@gmail.com>
2025-05-31 07:48:42 +0100

    Expand source command

--- .config/fish/conf.d/aliases/utility.fish
+++ .config/fish/conf.d/aliases/utility.fish
@@ -3,7 +3,7 @@ set NEOVIM_CONFIGURATION_PATH "$HOME/.config/nvim/"
 
 alias l="ls -al"
 alias q="exit"
-alias r="clear && . $FISH_CONFIG_PATH"
+alias r="clear && source $FISH_CONFIG_PATH"
 alias schmod="sudo chmod"
 alias schown="sudo chown"
 alias sshq="ssh -q"
