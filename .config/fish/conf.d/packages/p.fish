39288ab7d4d9920f81f2e761a48f5bde77f9f2d2
cyrus01337 <cyrus01337@gmail.com>
2025-06-03 09:32:15 +0100

    Prefer "web-development" over "javascript"

--- .config/fish/conf.d/packages/p.fish
+++ .config/fish/conf.d/packages/p.fish
@@ -4,7 +4,7 @@ set P_LOGS
 set P_LOADED
 set P_FILE (status --current-filename)
 set P_DIRECTORY (dirname $P_FILE)
-set P_SUPPORTED_LANGUAGES "javascript"
+set P_SUPPORTED_LANGUAGES "web-development"
 set P_SUPPORTED_SYSTEM_PACKAGE_MANAGERS "dnf"
 
 if not set -q P_DISABLE_LOGGING
