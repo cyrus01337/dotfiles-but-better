91673e6b1c1598ed1f728400a022728112a539d2
Cyrus <63274991+cyrus01337@users.noreply.github.com>
2025-06-04 13:19:22 +0100

    PIpe password with warning

--- .config/arch/install.sh
+++ .config/arch/install.sh
@@ -35,8 +35,10 @@ echo -n "en_GB.UTF-8 UTF-8" > /mnt/etc/locale.gen && \
     echo -n "arch" > /mnt/etc/hostname && \
     arch-chroot /mnt locale-gen
 
-arch-chroot /mnt useradd -m cyrus && \
-    passwd cyrus && \
+arch-chroot /mnt useradd -m cyrus
+# this script is to automate guest installations where snapshots fail,
+# never do this for public-facing/online systems where security matters
+echo "balls" | passwd cyrus --stdin && \
     echo "cyrus ALL=(ALL) NOPASSWD: ALL" >> /mnt/etc/sudoers
 
 arch-chroot /mnt systemctl enable NetworkManager sddm vmtoolsd
