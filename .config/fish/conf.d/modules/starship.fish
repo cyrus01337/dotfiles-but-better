4d63e6bcd5638a1be8fdc6e43c96e0a011ff3a49
cyrus01337 <cyrus01337@gmail.com>
2025-06-12 20:07:29 +0100

    Setup transient prompt

--- .config/fish/conf.d/modules/starship.fish
+++ .config/fish/conf.d/modules/starship.fish
@@ -2,7 +2,16 @@
 set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
 
 if command -q starship
+    function starship_transient_prompt_func
+        starship module character
+    end
+
+    function starship_transient_rprompt_func
+        starship module time
+    end
+
     starship init fish | source
+    enable_transience
 end
 
 return 0
