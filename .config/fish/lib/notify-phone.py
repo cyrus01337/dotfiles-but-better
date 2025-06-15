b6fd205732398b6b773e4b39b43167771b8bdac7
cyrus01337 <cyrus01337@gmail.com>
2025-06-15 07:02:13 +0100

    Ignore commands when also used with sudo

--- .config/fish/lib/notify-phone.py
+++ .config/fish/lib/notify-phone.py
@@ -17,29 +17,33 @@ class ConfigurationError(Exception):
     pass
 
 
+def running_ignored_command(command: str, ignored_commands: set[str]) -> bool:
+    head, maybe_command, *_ = command.split(" ")
+
+    for ignored in ignored_commands:
+        if head.startswith(ignored) or head.startswith("sudo") and maybe_command.startswith(ignored):
+            return True
+
+    return False
+
+
 def main():
     with ENVIRONMENT_FILE.open("rb") as file_header:
         environment = tomllib.load(file_header)
         webhook_url = environment["WEBHOOK_URL"]
 
         if not URL_REGEX.match(webhook_url):
-            raise ConfigurationError(
-                f'WEBHOOK_URL "{webhook_url}" does not match the following regex: {URL_REGEX.pattern}'
-            )
+            raise ConfigurationError(f'WEBHOOK_URL "{webhook_url}" does not match the following regex: {URL_REGEX.pattern}')
 
         command, duration, status = sys.argv[1:]
 
-        if (ignored_commands_found := environment.get("IGNORED_COMMANDS", None)) and (
-            _starts_with_ignored_command := any(
-                command.startswith(ignored_command)
-                for ignored_command in ignored_commands_found
-            )
-        ):
-            exit(1)
+        if ignored_commands_found := environment.get("IGNORED_COMMANDS", None):
+            ignored_commands = set(ignored_commands_found)
 
-        formatted_duration = humanize.naturaldelta(
-            datetime.timedelta(milliseconds=int(duration))
-        )
+            if running_ignored_command(command, ignored_commands):
+                exit(1)
+
+        formatted_duration = humanize.naturaldelta(datetime.timedelta(milliseconds=int(duration)))
         json = {
             "content": f"`{command}` ran for {formatted_duration} with exit code `{status}`",
         }
