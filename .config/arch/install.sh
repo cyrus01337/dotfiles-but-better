7b0e340edce4903d38fcfa51df084086176d6e45
Cyrus <63274991+cyrus01337@users.noreply.github.com>
2025-06-15 02:04:04 +0100

    Terminate sed commands

--- .config/arch/install.sh
+++ .config/arch/install.sh
@@ -45,8 +45,8 @@ timedatectl set-timezone Europe/London && \
 
 sed -i -E "s/^#(Color|ParallelDownloads.+)/\1/g" /etc/pacman.conf && \
     sed -i "s/!ccache/ccache/" /etc/makepkg.conf && \
-    sed -i -E "s/#MAKEFLAGS=.*/MAKEFLAGS='--jobs=\$(nproc)'" /etc/makepkg.conf && \
-    sed -i -E 's/RUSTFLAGS="(.*)"/RUSTFLAGS="\1 -C link-arg=-fuse-ld=mold"' /etc/makepkg.conf.d/rust.conf && \
+    sed -i -E "s/#MAKEFLAGS=.*/MAKEFLAGS='--jobs=\$(nproc)'/" /etc/makepkg.conf && \
+    sed -i -E 's/RUSTFLAGS="(.*)"/RUSTFLAGS="\1 -C link-arg=-fuse-ld=mold/"' /etc/makepkg.conf.d/rust.conf && \
     echo "LDFLAGS+=' -fuse-ld=mold'" >> /etc/makepkg.conf && \
     pacman -Sy --needed --noconfirm archlinux-keyring && \
     pacman -S --needed --noconfirm ccache mold
