f4eac9bee320ca6d4a6e6d9e25c8fad6cc776fa7
cyrus01337 <cyrus01337@gmail.com>
2025-07-29 20:06:08 +0100

    Add systemd interface abbreviations

--- .config/fish/conf.d/1-modules/utility.fish
+++ .config/fish/conf.d/1-modules/utility.fish
@@ -1,10 +1,12 @@
 #!/usr/bin/env fish
 abbr c "clear"
+abbr --position=anywhere jc "journalctl"
 abbr l "ls -al"
 abbr nuke "rm -rf ./{*,.*}"
 abbr q "exit"
 abbr r "clear && source $__fish_config_dir/config.fish"
 abbr --position=anywhere s "sudo"
+abbr --position=anywhere sc "systemctl"
 abbr scpq "scp &> /dev/null"
 abbr se "sudoedit"
 abbr sshq "ssh -q"
