ec29d05fc0707a16b2f8783ba000da545761f844
cyrus01337 <cyrus01337@gmail.com>
2025-07-29 23:18:05 +0100

    Query for package

--- .config/fish/conf.d/1-modules/yay.fish
+++ .config/fish/conf.d/1-modules/yay.fish
@@ -2,6 +2,7 @@
 if command -q yay
     abbr y "yay"
     abbr yi "yay -S --needed --noconfirm"
+    abbr yq "yay -Qi"
     abbr yrm "yay -Rns --noconfirm"
     abbr ysr "yay -Ss"
     abbr ysu "yay -Syu --noconfirm"
