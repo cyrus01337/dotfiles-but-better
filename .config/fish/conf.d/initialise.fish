cd6da6e13bad07db5061e4bcc313f1dbb8fab555
cyrus01337 <cyrus01337@gmail.com>
2025-07-29 21:08:40 +0100

    Show boot times on startup

--- .config/fish/conf.d/initialise.fish
+++ .config/fish/conf.d/initialise.fish
@@ -2,3 +2,9 @@
 for script in $__fish_config_dir/conf.d/{*/*, */*/*}.fish
     source $script
 end
+
+if command -q systemd-analyze; and not set -q INITIALISED
+    systemd-analyze
+end
+
+set INITIALISED true
