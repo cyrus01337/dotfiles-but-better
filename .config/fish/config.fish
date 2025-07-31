927840fa640b36ade6d063b9246f27de91f51f7e
cyrus01337 <cyrus01337@gmail.com>
2025-07-31 21:47:27 +0100

    Comment disabling command timer

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 set fish_greeting
+# set -U DISABLE_COMMAND_TIMER true
 
 fish_add_path "$HOME/bin" "$HOME/bin/custom" "/snap/bin" "$HOME/.local/bin" "/opt" "$HOME/.spicetify"
 fish_config theme choose "Dracula Official"
