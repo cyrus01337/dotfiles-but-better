228db0de4197cfae74d1811aa3ab92da569be8c1
cyrus01337 <cyrus01337@gmail.com>
2025-08-09 02:04:30 +0100

    Allow nuke to run anywhere in command

--- .config/fish/conf.d/1-modules/utility.fish
+++ .config/fish/conf.d/1-modules/utility.fish
@@ -2,7 +2,7 @@
 abbr c "clear"
 abbr --position=anywhere jc "journalctl"
 abbr l "ls -al"
-abbr nuke "rm -rf ./{*,.*}"
+abbr --position=anywhere nuke "rm -rf"
 abbr q "exit"
 abbr r "clear && source $__fish_config_dir/config.fish"
 abbr --position=anywhere s "sudo"
