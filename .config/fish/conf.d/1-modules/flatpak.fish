be5f674e7df4c35864be72dbaa97b47ba14cbb13
cyrus01337 <cyrus01337@gmail.com>
2025-08-12 10:12:59 +0100

    Add Flatpak command abbreviations

--- .config/fish/conf.d/1-modules/flatpak.fish
+++ .config/fish/conf.d/1-modules/flatpak.fish
@@ -1,6 +1,11 @@
 #!/usr/bin/env fish
 if command -q flatpak
     fish_add_path "/var/lib/flatpak/exports/bin"
+
+    abbr f "flatpak"
+    abbr fi "flatpak install -y"
+    abbr fu "flatpak uninstall -y"
+    abbr --set-cursor fp "flatpak uninstall -y % && flatpak uninstall --unused"
 end
 
 return 0
