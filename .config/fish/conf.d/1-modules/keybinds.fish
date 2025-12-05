50b87bfc244c3fea7eab937a43298a3eb8e05c6b
cyrus01337 <cyrus01337@gmail.com>
2025-12-05 13:25:27 +0000

    Ensure tab completion allows cycling in the right direction

--- .config/fish/conf.d/1-modules/keybinds.fish
+++ .config/fish/conf.d/1-modules/keybinds.fish
@@ -2,6 +2,12 @@
 bind \e\[7\;5~ "backward-kill-word"
 bind \e\[1\;5H "beginning-of-buffer"
 bind \e\[1\;5F "end-of-buffer"
-bind \t "complete-and-search"
+bind -M insert tab '
+    if commandline --search-field >/dev/null
+        commandline -f complete
+    else
+        commandline -f complete-and-search
+    end
+'
 
 return 0
