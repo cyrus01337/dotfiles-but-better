2a03e6e0e73c60b70a343030a9c4cb8e108bd186
cyrus01337 <cyrus01337@gmail.com>
2026-01-27 02:01:06 +0000

    Use new session plugin

--- .config/nvim/lua/custom/features/sessions.lua
+++ .config/nvim/lua/custom/features/sessions.lua
@@ -1,23 +1,18 @@
-local function auto_create() end
-
-local function oil()
-    require("oil")
-end
-
 return {
-    "rmagatti/auto-session",
-    lazy = false,
+    "folke/persistence.nvim",
+    event = "BufReadPre",
     init = function()
+        local persistence = require("persistence")
+
         vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+        persistence.load()
     end,
     opts = {
-        args_allow_files_auto_save = true,
-        auto_create = auto_create,
-        git_use_branch_name = true,
-        git_auto_restore_on_branch_change = true,
-        no_restore_cmds = { oil },
-        session_lens = {
-            load_on_setup = false,
-        },
+        need = 0,
+        options = { "globals" },
+        pre_save = function()
+            vim.api.nvim_exec_autocmds("User", { pattern = "SessionSavePre" })
+        end,
     },
 }
