235947f62c34730af6ff1a23ad1959757a22527e
cyrus01337 <cyrus01337@gmail.com>
2026-03-25 13:22:52 +0000

    Rename

--- services/notification-server/install.sh
+++ services/notification-server/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-SERVICE_NAME="notifications-server.service"
+SERVICE_NAME="notification-server.service"
 
 docker compose up -d
 sudo ln -s $(realpath $SERVICE_NAME) /usr/lib/systemd/system/$SERVICE_NAME
 sudo ln -s $(realpath $SERVICE_NAME) /etc/systemd/system/$SERVICE_NAME
+sudo systemctl enable --now $SERVICE_NAME
